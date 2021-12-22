b71165f21c0e8cde5de8c8957f154324a1c413a9
Jorch <jorch.rl@gmail.com>
2021-12-22 17:00:24 -0600

    Add MongoDB connection to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,26 @@ import express from "express";
 import devBundle from "./devBundle";
 import path from "path";
 import template from "./../template";
+import { MongoClient } from "mongodb";
+
+const url = process.env.MONGO_URI || "mongodb://localhost:27017/mernBase";
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, db) => {
+    if (err) {
+      console.log("Not connected to MongoDB :(");
+      try {
+        db.close();
+      } catch (error) {
+        console.log("No db to close.");
+      }
+      return;
+    }
+    console.log("Connected to MongoDB");
+    db.close();
+  }
+);
 
 const CURRENT_WORKING_DIR = process.cwd();
 
